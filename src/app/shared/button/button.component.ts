e5880f380574b93920bdbf5924ab465969734df0
garrettpyke <garrettpyke@msn.com>
2024-07-05 15:25:36 -0600

    Factor out a shared Control component

--- src/app/shared/button/button.component.ts
+++ src/app/shared/button/button.component.ts
@@ -2,7 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
   // syntax for an element[attribute] selector - see ng docs on selectors
-  selector: "button[appButton]",
+  // the anchor element is not used...just illustrates syntax for multiple selectors
+  selector: "button[appButton], a[appButton]",
   standalone: true,
   imports: [],
   templateUrl: "./button.component.html",
