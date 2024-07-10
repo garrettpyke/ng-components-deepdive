ef8c1bb52e765c5eddfdcab368372091b867d45e
garrettpyke <garrettpyke@msn.com>
2024-07-10 16:08:20 -0600

    First use of Template Values as opposed to 2-way binding

--- src/app/dashboard/support-tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/support-tickets/new-ticket/new-ticket.component.ts
@@ -1,13 +1,19 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
-import { ControlComponent } from "./../../../shared/control/control.component";
-import { ButtonComponent } from "./../../../shared/button/button.component";
+import { ControlComponent } from './../../../shared/control/control.component';
+import { ButtonComponent } from './../../../shared/button/button.component';
 
 @Component({
-  selector: "app-new-ticket",
+  selector: 'app-new-ticket',
   standalone: true,
-  imports: [ControlComponent, ButtonComponent],
-  templateUrl: "./new-ticket.component.html",
-  styleUrl: "./new-ticket.component.css",
+  imports: [ControlComponent, ButtonComponent, FormsModule],
+  templateUrl: './new-ticket.component.html',
+  styleUrl: './new-ticket.component.css',
 })
-export class NewTicketComponent {}
+export class NewTicketComponent {
+  onSubmit(titleElement: HTMLInputElement) {
+    console.dir(titleElement);
+    console.log('Form submitted, value: ' + titleElement.value);
+  }
+}
